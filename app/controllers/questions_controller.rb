204d9c2cb6bc7ee26ab3b1ec26140b1396564da6
Andex <anyanov1998@gmail.com>
2021-08-03 17:58:07 +0300

    Add: спеки для action create вопросов valid

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -12,7 +12,18 @@ class QuestionsController < ApplicationController
     @question = Question.new
   end
 
+  def create
+    @question = Question.create(question_params)
+    redirect_to @question
+  end
+
   def edit
     @question = Question.find(params[:id])
+  end
+
+  private
+
+  def question_params
+    params.require(:question).permit(:title, :body)
   end
 end
