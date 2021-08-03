e3ebaba79f6c40f8d7a5481178cfc3b1fd875515
Andex <anyanov1998@gmail.com>
2021-08-03 23:53:58 +0300

    Add: action create для ответов на вопрос

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -5,9 +5,23 @@ class AnswersController < ApplicationController
     @answer = @question.answers.new
   end
 
+  def create
+    @answer = @question.answers.new(answer_params)
+
+    if @answer.save
+      redirect_to @answer
+    else
+      render :new
+    end
+  end
+
   private
 
   def load_question
     @question = Question.find(params[:question_id])
+  end
+
+  def answer_params
+    params.require(:answer).permit(:body, :question_id)
   end
 end
