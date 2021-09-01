ee3fd575c590a4ca362193f8623d350078d79219
Andex <anyanov1998@gmail.com>
2021-09-01 23:14:02 +0300

    Add: метод отметки ответа лучшим

--- spec/models/answer_spec.rb
+++ spec/models/answer_spec.rb
@@ -5,4 +5,16 @@ RSpec.describe Answer, type: :model do
   it { should belong_to(:user) }
 
   it { should validate_presence_of :body }
+
+  describe '#mark_as_best' do
+    let(:question) { create(:question) }
+    let(:answer) { create(:answer, question: question) }
+
+    it 'the best answer is chosen' do
+      answer.mark_as_best
+
+      expect(question.best_answer).to eq answer
+    end
+
+  end
 end
