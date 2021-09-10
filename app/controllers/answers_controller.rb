39c8f14714035970b2ccd9e7b1a32d95bd432cb7
Andex <anyanov1998@gmail.com>
2021-09-10 17:03:11 +0300

    Add: прикрепление файлов при создании ответа

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -44,6 +44,6 @@ class AnswersController < ApplicationController
   end
 
   def answer_params
-    params.require(:answer).permit(:body, :question_id)
+    params.require(:answer).permit(:body, :question_id, files: [])
   end
 end
