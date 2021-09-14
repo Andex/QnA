76ed6d71154f6d8b582df798edbb5afa662c8c35
Andex <anyanov1998@gmail.com>
2021-09-15 00:16:42 +0300

    Add: спека на прикрепление ссылки при создании ответа

---
+++ spec/features/answer/add_links_spec.rb
@@ -0,0 +1,32 @@
+require 'rails_helper'
+
+feature 'User can add links to answer', %q{
+  In order to provide additional info to my answer
+  As an answer's author
+  I'd like to be able to add links
+} do
+
+  given(:user) { create(:user) }
+  given(:question) { create(:question) }
+  given(:gist_url) { 'https://gist.github.com/Andex/adaaec3cd7d9e72b9e4d3a89465aeb34' }
+
+  scenario 'User adds link when asks answer' do
+    login(user)
+    visit question_path(question)
+
+    fill_in 'Your answer', with: 'My answer'
+
+    fill_in 'Link name', with: 'My gist'
+    fill_in 'Url', with: gist_url
+
+    click_on 'To answer'
+
+    within '.answers' do
+      expect(page).to have_link 'My gist', href: gist_url
+    end
+  end
+
+  scenario 'User adds link when asks answer with errors' do
+
+  end
+end
