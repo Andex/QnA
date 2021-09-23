bca0130aa61d9e04d194859b00ad7c7c67b2590a
Andex <anyanov1998@gmail.com>
2021-09-23 20:35:00 +0300

    Add: модель Vote

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,6 +7,7 @@ class User < ApplicationRecord
   has_many :questions, dependent: :destroy
   has_many :answers, dependent: :destroy
   has_many :rewards, dependent: :nullify
+  has_many :votes, dependent: :destroy
 
   def is_author?(obj)
     obj.user_id == id
---
+++ app/models/vote.rb
@@ -0,0 +1,7 @@
+class Vote < ApplicationRecord
+  belongs_to :user
+  belongs_to :votable, polymorphic: true
+
+  validates :value, presence: true, numericality: { only_integer: true }
+  validates :user, uniqueness: { scope: %i[votable_id votable_type] }
+end
