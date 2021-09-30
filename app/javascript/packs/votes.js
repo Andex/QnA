db6c568af787dea288a8c1d3c263e094f2a42984
Andex <anyanov1998@gmail.com>
2021-09-30 22:53:06 +0300

    DeleteUnnecessary.

--- app/javascript/packs/votes.js
+++ app/javascript/packs/votes.js
@@ -2,16 +2,11 @@ $(document).on('turbolinks:load', function(){
     $('.cancel-vote-link, .vote-up-link, .vote-down-link').on('ajax:success', function(e) {
         var vote = e.detail[0];
 
-        // var resource = $('.' + vote.resource + '-id-' + vote.id)
-        // console.log(vote)
         if(vote.resource === 'question'){
             var resource = $('.question')
         } else {
             var resource = $('.answer-id-' + vote.id)
         }
-        // console.log(e.detail)
-        // console.log(resource)
-        // console.log(resource.find('.rating .value'))
         resource.find('.rating .value').html(vote.rating);
 
         resource.find('.vote-up-link, .vote-down-link').toggle();
