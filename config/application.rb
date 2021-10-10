b276ea0596dbdbba5af1b851a5176d984fd32290
Andex <anyanov1998@gmail.com>
2021-10-10 21:11:28 +0300

    Add: подгрузка папки services при загрузке приложения

--- config/application.rb
+++ config/application.rb
@@ -26,5 +26,7 @@ module QnA
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+
+    config.autoload_paths << Rails.root.join('app')
   end
 end
