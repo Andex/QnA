c0e34902553d95339d5fd2998a8db1592878e4e9
Andex <anyanov1998@gmail.com>
2021-10-11 14:43:52 +0300

    Refactor: shared examples спеки и контроллера oauth_callbacks

--- app/controllers/oauth_callbacks_controller.rb
+++ app/controllers/oauth_callbacks_controller.rb
@@ -1,12 +1,25 @@
 class OauthCallbacksController < Devise::OmniauthCallbacksController
   def github
-    @user = User.find_for_oauth(request.env['omniauth.auth'])
+    connect_to('Github')
+  end
+
+  def connect_to(provider)
+    auth = request.env['omniauth.auth']
+    @user = User.find_for_oauth(auth)
 
     if @user&.persisted?
       sign_in_and_redirect @user, event: :authentication
-      set_flash_message(:notice, :success, kind: 'Github') if is_navigational_format?
+      set_flash_message(:notice, :success, kind: provider) if is_navigational_format?
+    elsif !auth.nil?
+      # Removing extra as it can overflow some session stores
+      session["devise.#{provider}_data"] = auth.except('extra')
+      redirect_to new_user_registration_path, alert: 'Something went wrong. Please try again later.'
     else
-      redirect_to root_path, alert: 'Something went wrong'
+      redirect_to new_user_registration_path, alert: 'Something went wrong. Please try again later.'
     end
   end
+
+  # def failure
+  #   redirect_to new_user_registration_path
+  # end
 end
--- spec/controllers/oauth_callbacks_controller_spec.rb
+++ spec/controllers/oauth_callbacks_controller_spec.rb
@@ -5,48 +5,53 @@ RSpec.describe OauthCallbacksController, type: :controller do
     @request.env["devise.mapping"] = Devise.mappings[:user]
   end
 
+  shared_examples 'Providers' do |provider|
+    describe "#{provider}" do
+      let(:oauth_data) { {'provider' => provider, 'uid' => 123 } }
+
+      it 'finds user from oauth data' do
+        allow(request.env).to receive(:[]).and_call_original
+        allow(request.env).to receive(:[]).with('omniauth.auth').and_return(oauth_data)
+        expect(User).to receive(:find_for_oauth).with(oauth_data)
+        get provider.to_sym
+      end
+
+      context 'user exists' do
+        let!(:user) { create(:user) }
+
+        before do
+          allow(User).to receive(:find_for_oauth).and_return(user)
+          get provider.to_sym
+        end
+
+        it 'login user' do
+          expect(subject.current_user).to eq user
+        end
+
+        it 'redirects to root path' do
+          expect(response).to redirect_to root_path
+        end
+      end
+
+      context 'user does not exist' do
+        before do
+          allow(User).to receive(:find_for_oauth)
+          get provider.to_sym
+        end
+
+        it 'redirects to registration path' do
+          expect(response).to redirect_to new_user_registration_path
+        end
+
+
+        it 'does not login user' do
+          expect(subject.current_user).to_not be
+        end
+      end
+    end
+  end
+
   describe 'Github' do
-    let(:oauth_data) { {'provider' => 'github', 'uid' => 123 } }
-
-    it 'finds user from oauth data' do
-      allow(request.env).to receive(:[]).and_call_original
-      allow(request.env).to receive(:[]).with('omniauth.auth').and_return(oauth_data)
-      expect(User).to receive(:find_for_oauth).with(oauth_data)
-      get :github
-    end
-
-    context 'user exists' do
-      let!(:user) { create(:user) }
-
-      before do
-        allow(User).to receive(:find_for_oauth).and_return(user)
-        get :github
-      end
-
-      it 'login user' do
-        expect(subject.current_user).to eq user
-      end
-
-
-      it 'redirects to root path' do
-        expect(response).to redirect_to root_path
-      end
-    end
-
-    context 'user does not exist' do
-      before do
-        allow(User).to receive(:find_for_oauth)
-        get :github
-      end
-
-      it 'redirects to root path' do
-        expect(response).to redirect_to root_path
-      end
-
-
-      it 'does not login user' do
-        expect(subject.current_user).to_not be
-      end
-    end
+    it_behaves_like 'Providers', 'github'
   end
 end
