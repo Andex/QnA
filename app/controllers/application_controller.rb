e185dee8dfa4a565d6a9685440c21707ead59e26
Andex <anyanov1998@gmail.com>
2021-10-17 21:59:39 +0300

    Fix: ответы для разных форматов при исключении CanCan::AccessDenied

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,10 +2,11 @@ class ApplicationController < ActionController::Base
   before_action :gon_user
 
   rescue_from CanCan::AccessDenied do |exception|
-    redirect_to root_url, alert: exception.message
-    # format.json { head :forbidden, content_type: 'text/html' }
-    # format.html { redirect_to root_path, alert: exception.message }
-    # format.js { head :forbidden, content_type: 'text/html' }
+    respond_to do |format|
+      format.json { head :forbidden }
+      format.html { redirect_to root_path, alert: exception.message }
+      format.js { head :forbidden }
+    end
   end
 
   check_authorization unless: :devise_controller?
