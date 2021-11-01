01b006e70313b63581224ecd01d0d3e0094828e8
Andex <anyanov1998@gmail.com>
2021-11-01 22:24:50 +0300

    Spec: отправка писем DailyDigest

---
+++ spec/services/daily_digest_spec.rb
@@ -0,0 +1,10 @@
+require 'rails_helper'
+
+Rspec.describe DailyDigestServices do
+  let(:users){ create_list(:user, 3) }
+
+  it 'sends daily digest to all users' do
+    users.each { |user| expect(DailyDigestMailer).to receive(:digest).with(user).and_call_original }
+    subject.send_digest
+  end
+end
