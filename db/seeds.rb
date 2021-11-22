539a5156c0ca8705a1e115b5673ad0bbb9c8b196
Andex <anyanov1998@gmail.com>
2021-11-22 18:54:04 +0300

    Add: seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,105 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+
+admin = User.create!(email: 'admin@admin.ru',
+                     password: '121212',
+                     confirmed_at: Time.now,
+                     admin: true)
+
+users = []
+4.times do
+  users << User.create(
+    email: Faker::Internet.email,
+    password: Faker::Internet.password(min_length: 8),
+    confirmed_at: Time.now
+  )
+end
+
+questions = []
+questions << Question.create([
+             { title: 'What movie / book is this phrase from? ', body: Faker::Books::Dune.quote, user: users.sample },
+             { title: 'What movie / book is this phrase from? ', body: Faker::Movies::BackToTheFuture.quote, user: users.sample },
+             { title: 'Back to the Future films', body: 'What dates did Doc and Marty travel on in the Back to the Future films?', user: users.sample },
+             { title: 'Error occurred while installing mini_racer', body: 'Gem::Ext::BuildError: ERROR: Failed to build gem native extension.', user: users.sample },
+])
+
+# Create rewards for few questions
+rewards = %w[spec/files/reward.png spec/files/reward_2.jpeg spec/files/reward_3.jpg]
+
+6.times do
+  reward = Reward.create(title: Faker::Quotes::Chiquito.term)
+  reward.image.attach(io: File.open(rewards.sample), filename: 'reward from question')
+  reward.question = questions.sample
+  reward.save!
+end
+
+answers = []
+3.times do
+  answers << Answer.create([
+             { body: Faker::Book, question: Question.first, user: users.sample },
+             { body: Faker::Movie, question: Question.second, user: users.sample },
+             { body: Faker::Movies::BackToTheFuture.date, question: Question.third, user: users.sample },
+             { body: Faker::Hacker.say_something_smart, question: Question.last, user: users.sample }
+  ])
+end
+
+# Create few comments for questions and answers
+questions.each do |question|
+  # Set a random answer to the best
+  question.answers.sample.best
+
+  rand(1..3).times do
+    question.comments.create(
+      body: Faker::Quote,
+      user: users.sample
+    )
+  end
+  rand(2..5).times do
+    question.answers.sample.comments.create(
+      body: Faker::Quote,
+      user: users.sample
+    )
+  end
+end
+
+10.times do
+  questions.sample.files.attach(io: File.open(rewards.sample), filename: 'file for question')
+  answers.sample.files.attach(io: File.open(rewards.sample), filename: 'file for answer')
+end
+
+# Create links for questions
+8.times do
+  subject = Faker::Verb
+  questions.sample.links.create(
+    name: subject,
+    url: "https://yandex.ru/images/search?utm_source=main_stripe_big&text=#{subject}"
+  )
+  questions.sample.links.create(
+    name: 'gist link',
+    url: 'https://gist.github.com/Andex/7f12685131f53e148d71a5d5ac4495f3'
+  )
+end
+
+# Create links for answers
+10.times do
+  subject = Faker::Verb
+  answers.sample.links.create(
+    name: "link to picture #{subject}",
+    url: "https://yandex.ru/images/search?utm_source=main_stripe_big&text=#{subject}"
+  )
+  answers.sample.links.create(
+    name: 'gist link',
+    url: 'https://gist.github.com/Andex/7f12685131f53e148d71a5d5ac4495f3'
+  )
+end
+
+# Simulate rating for questions
+10.times do
+  questions.sample.vote(1, users.sample)
+  questions.sample.vote(-1, users.sample)
+end
+
+# Simulate rating for answers
+10.times do
+  answers.sample.vote(1, users.sample)
+  answers.sample.vote(-1, users.sample)
+end
