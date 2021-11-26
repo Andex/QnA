f80f3fb5fcf0af86e8ef704741c486678afe5d25
Andex <anyanov1998@gmail.com>
2021-11-26 20:34:47 +0300

    Fix: отображение новых комментариев на стр. др поль-лей в реальном времени

--- app/javascript/channels/comments_channel.js
+++ app/javascript/channels/comments_channel.js
@@ -3,6 +3,12 @@ import consumer from "./consumer"
 consumer.subscriptions.create({ channel: "CommentsChannel", question_id: gon.question_id }, {
     received(data) {
         if(data.comment.user_id !== gon.current_user_id){
+            let header = $('.' + data.comment.commentable_type.toLowerCase() + '-' + data.comment.commentable_id + '-comments .list')
+                .find('.fs-6')
+            if(header.length == 0) {
+                $('.' + data.comment.commentable_type.toLowerCase() + '-' + data.comment.commentable_id + '-comments .list')
+                    .append('<div class="fs-6">Comments:</div>');
+            }
             $('.' + data.comment.commentable_type.toLowerCase() + '-' + data.comment.commentable_id + '-comments .list')
                 .append('<p>' + data.comment.body + ' ©' + data.user_email + '</p>')
         }
