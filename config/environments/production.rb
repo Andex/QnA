6565f65ae6e8aaeb50bd5dd3444428299bfa45cc
Andex <anyanov1998@gmail.com>
2021-12-03 00:08:13 +0300

    Fix: имя домена для конфигурации gmail

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -69,7 +69,7 @@ Rails.application.configure do
   config.action_mailer.smtp_settings = {
     address:              'smtp.gmail.com',
     port:                 587,
-    domain:               'example.com',
+    domain:               'qna-andex.club',
     user_name:            Rails.application.credentials.dig(:production, :gmail, :smtp_username),
     password:             Rails.application.credentials.dig(:production, :gmail, :smtp_password),
     authentication:       'plain',
