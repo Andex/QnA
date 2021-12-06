93b7db48199f8967342929ab03141e54f8f0d51e
Andex <anyanov1998@gmail.com>
2021-12-06 22:47:02 +0300

    Fix: удаление награды и ссылок админом

--- app/controllers/links_controller.rb
+++ app/controllers/links_controller.rb
@@ -5,7 +5,6 @@ class LinksController < ApplicationController
 
   def destroy
     @link = Link.find(params[:id])
-    return unless current_user&.is_author?(@link.linkable)
 
     @link.destroy
     flash.now.notice = 'Your link was deleted.'
